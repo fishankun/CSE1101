5b5ec7ba0dc1fbf0a821feb05380530c1c318994
Shafin <nasif.dnj@gmail.com>
2026-08-09 16:08:32 +0600

    added matrix multiplication with pointer only

---
+++ assignment(L)/pointer_to_pointer/matrix_multiplication.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int row1, row2, col1, col2, i, j, a, b;
+    printf("Enter row no. for 1st matrix: ");
+    scanf("%d", &row1);
+    printf("Enter col no. for 1st matrix: ");
+    scanf("%d", &col1);
+    printf("Enter row no. for 2nd matrix: ");
+    scanf("%d", &row2);
+    printf("Enter col no. for 2nd matrix: ");
+    scanf("%d", &col2);
+    if(col1 != row2){
+        printf("Invalid Input\n");
+        return 0;
+    }
+
+    int **mat1, **mat2;
+
+    //scaning the first array
+    mat1 = (int**)malloc(row1 * sizeof(int*));
+    for(i = 0; i < row1; i++){
+        *(mat1 + i) = (int*)malloc(col1 * sizeof(int));
+    }
+    printf("Enter the first matrix:\n");
+    for(i = 0; i < row1; i++){
+        for(j = 0; j < col1; j++){
+            scanf("%d", *(mat1 + i) + j);
+        }
+    }
+
+    //scanning the second array
+    mat2 = (int**)malloc(row2 * sizeof(int*));
+    for(i = 0; i < row2; i++){
+        *(mat2 + i) = (int*)malloc(col2 * sizeof(int));
+    }
+    printf("Enter the second matrix:\n");
+    for(i = 0; i < row2; i++){
+        for(j = 0; j < col2; j++){
+            scanf("%d", *(mat2 + i) + j);
+        }
+    }
+
+
+    for(a = 0; a < row1; a++){
+        for(b = 0; b < col2; b++){
+            int sum = 0;
+            for(i = 0; i < col1; i++){
+                // mat[a][b] = *(*(mat + a) + b)
+                // we are going to the a-th position of the row and taking its base address. then we go to the b-th position of that base address or the column.
+                // then taking the value of that position.
+                sum = sum + (*(*(mat1 + a) + i)) * (*(*(mat2 + i) + b));
+            }
+            printf("%d ", sum);
+        }
+        printf("\n");
+    }
+
+
+
+    mat1 = (int**)malloc(row1 * sizeof(int*));
+    for(i = 0; i < row1; i++){
+        free(*(mat1 + i));
+    }
+    free(mat1);
+    mat2 = (int**)malloc(row2 * sizeof(int*));
+    for(i = 0; i < row2; i++){
+        free(*(mat2 + i));
+    }
+    free(mat2);
+}
